8a7a616fddea2810d8dfaaff787c31443029ff22
wanrong <zhuwr@pku.edu.cn>
2017-11-29 10:53:06 +0800

    deleted:    TCP-IP/main.cpp 	deleted:    TCP-IP/SenderIP.hpp 	deleted:    TCP-IP/SenderTCP.hpp 	modified:   TCP-IP/packet.hh 	modified:   TCP-IP/receiverbuffer.cc 	modified:   TCP-IP/receiverbuffer.hh 	modified:   TCP-IP/receivertcp.cc 	modified:   TCP-IP/senderbuffer.cc 	modified:   TCP-IP/senderbuffer.hh

---
+++ TCP-IP/SenderIP.hpp
@@ -0,0 +1,14 @@
+//
+//  SenderIP.hpp
+//  TCP-IP
+//
+//  Created by 朱芄蓉 on 13/11/2017.
+//  Copyright © 2017 朱芄蓉. All rights reserved.
+//
+
+#ifndef SenderIP_hpp
+#define SenderIP_hpp
+
+#include <stdio.h>
+
+#endif /* SenderIP_hpp */
---
+++ TCP-IP/SenderTCP.hpp
@@ -0,0 +1,14 @@
+//
+//  SenderTCP.hpp
+//  TCP-IP
+//
+//  Created by 朱芄蓉 on 13/11/2017.
+//  Copyright © 2017 朱芄蓉. All rights reserved.
+//
+
+#ifndef SenderTCP_hpp
+#define SenderTCP_hpp
+
+#include <stdio.h>
+
+#endif /* SenderTCP_hpp */
